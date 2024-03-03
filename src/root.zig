1231020f0c614b2d66501b21363e23fb730123dc
Luis Cáceres <lacc97@protonmail.ch>
2024-03-03 22:41:51 +0000

    Implement multi-threaded
    This is still a spsc queue.

--- src/root.zig
+++ src/root.zig
@@ -4,9 +4,31 @@ const assert = std.debug.assert;
 
 pub const Options = struct {
     safety_checks: bool = (builtin.mode == .Debug or builtin.mode == .ReleaseSafe),
+    single_threaded: bool = true,
 };
 
 fn BipBufferUnmanaged(comptime T: type, comptime opts: Options) type {
+    const load = struct {
+        inline fn load(p: *const usize) usize {
+            var v: usize = undefined;
+            if (comptime opts.single_threaded) {
+                v = p.*;
+            } else {
+                v = @atomicLoad(usize, p, .Acquire);
+            }
+            return v;
+        }
+    }.load;
+    const store = struct {
+        inline fn store(p: *usize, v: usize) void {
+            if (comptime opts.single_threaded) {
+                p.* = v;
+            } else {
+                @atomicStore(usize, p, v, .Release);
+            }
+        }
+    }.store;
+
     return struct {
         const Buffer = @This();
 
@@ -38,9 +60,9 @@ fn BipBufferUnmanaged(comptime T: type, comptime opts: Options) type {
         }
 
         pub fn totalAvailableForWrite(b: *const Buffer) usize {
-            const head = b.head;
+            const head = load(&b.head);
             if (comptime opts.safety_checks) if (!(head < b.data.len)) @panic("BUG: inconsistent head value");
-            const tail = b.tail;
+            const tail = load(&b.tail);
             if (comptime opts.safety_checks) if (!(tail < b.data.len)) @panic("BUG: inconsistent tail value");
 
             // This is the region that immediately follows after the current value of head, limited by either tail or the end of the buffer.
@@ -55,11 +77,11 @@ fn BipBufferUnmanaged(comptime T: type, comptime opts: Options) type {
         }
 
         pub fn totalAvailableForRead(b: *const Buffer) usize {
-            const mark = b.mark;
+            const mark = load(&b.mark);
             if (comptime opts.safety_checks) if (!(mark < b.data.len)) @panic("BUG: inconsistent mark value");
-            const head = b.head;
+            const head = load(&b.head);
             if (comptime opts.safety_checks) if (!(head < b.data.len)) @panic("BUG: inconsistent head value");
-            const tail = b.tail;
+            const tail = load(&b.tail);
             if (comptime opts.safety_checks) if (!(tail < b.data.len)) @panic("BUG: inconsistent tail value");
 
             // This is the region that immediately follows after the current value of tail, limited by head or mark.
@@ -76,9 +98,9 @@ fn BipBufferUnmanaged(comptime T: type, comptime opts: Options) type {
         pub fn reserve(b: *Buffer, count: usize) error{OutOfMemory}!Reservation {
             if (comptime opts.safety_checks) if (!(count > 0)) @panic("bad reserve count");
 
-            const head = b.head;
+            const head = load(&b.head);
             if (comptime opts.safety_checks) if (!(head < b.data.len)) @panic("BUG: inconsistent head value");
-            const tail = b.tail;
+            const tail = load(&b.tail);
             if (comptime opts.safety_checks) if (!(tail < b.data.len)) @panic("BUG: inconsistent tail value");
 
             // This is the region that immediately follows after the current value of head. It is limited by either tail or the end of the buffer.
@@ -105,9 +127,9 @@ fn BipBufferUnmanaged(comptime T: type, comptime opts: Options) type {
         pub fn commit(b: *Buffer, r: Reservation, count: usize) void {
             if (count == 0) return;
 
-            const mark = b.mark;
+            const mark = load(&b.mark);
             if (comptime opts.safety_checks) if (!(mark < b.data.len)) @panic("BUG: inconsistent mark value");
-            const head = b.head;
+            const head = load(&b.head);
             if (comptime opts.safety_checks) if (!(head < b.data.len)) @panic("BUG: inconsistent head value");
 
             if (comptime opts.safety_checks) if (!(r.data.len < b.data.len)) @panic("bad reservation");
@@ -120,23 +142,23 @@ fn BipBufferUnmanaged(comptime T: type, comptime opts: Options) type {
             const next_head = r.head + count;
             if (comptime opts.safety_checks) if (!(next_head < b.data.len)) @panic("BUG: inconsistent head value");
 
-            const tail = b.tail;
+            const tail = load(&b.tail);
             if (comptime opts.safety_checks) if (!(tail < b.data.len)) @panic("BUG: inconsistent tail value");
 
             // Set mark when wraparound, otherwise keep up with head.
             const next_mark = if (next_head <= head) (head) else @max(mark, next_head);
             if (comptime opts.safety_checks) if (!(next_mark >= tail)) @panic("BUG: inconsistent mark value");
 
-            b.mark = next_mark;
-            b.head = next_head;
+            store(&b.mark, next_mark);
+            store(&b.head, next_head);
         }
 
         pub fn peek(b: *const Buffer) []const u8 {
-            const mark = b.mark;
+            const mark = load(&b.mark);
             if (comptime opts.safety_checks) if (!(mark < b.data.len)) @panic("BUG: inconsistent mark value");
-            const head = b.head;
+            const head = load(&b.head);
             if (comptime opts.safety_checks) if (!(head < b.data.len)) @panic("BUG: inconsistent head value");
-            const tail = b.tail;
+            const tail = load(&b.tail);
             if (comptime opts.safety_checks) if (!(tail < b.data.len)) @panic("BUG: inconsistent tail value");
 
             const end = if (tail <= head) (head) else (mark);
@@ -146,11 +168,11 @@ fn BipBufferUnmanaged(comptime T: type, comptime opts: Options) type {
         pub fn consume(b: *Buffer, count: usize) void {
             if (count == 0) return;
 
-            const mark = b.mark;
+            const mark = load(&b.mark);
             if (comptime opts.safety_checks) if (!(mark < b.data.len)) @panic("BUG: inconsistent mark value");
-            const head = b.head;
+            const head = load(&b.head);
             if (comptime opts.safety_checks) if (!(head < b.data.len)) @panic("BUG: inconsistent head value");
-            const tail = b.tail;
+            const tail = load(&b.tail);
             if (comptime opts.safety_checks) if (!(tail < b.data.len)) @panic("BUG: inconsistent tail value");
 
             const end = if (tail <= head) (head) else (mark);
@@ -160,7 +182,7 @@ fn BipBufferUnmanaged(comptime T: type, comptime opts: Options) type {
             const next_tail = if (tail <= head or count < (end - tail)) (tail + count) else 0;
             if (comptime opts.safety_checks) if (!(next_tail < b.data.len)) @panic("BUG: inconsistent tail value");
 
-            b.tail = next_tail;
+            store(&b.tail, next_tail);
         }
     };
 }
@@ -168,64 +190,71 @@ fn BipBufferUnmanaged(comptime T: type, comptime opts: Options) type {
 test {
     const testing = std.testing;
 
-    const storage = try testing.allocator.alloc(u8, 17);
-    defer testing.allocator.free(storage);
+    const types = .{
+        BipBufferUnmanaged(u8, .{ .single_threaded = false }),
+        BipBufferUnmanaged(u8, .{ .single_threaded = true }),
+    };
 
-    var b = BipBufferUnmanaged(u8, .{}).init(storage);
-    try testing.expectEqual(@as(usize, 16), b.totalAvailableForWrite());
-    try testing.expectEqual(@as(usize, 0), b.totalAvailableForRead());
+    inline for (types) |B| {
+        const storage = try testing.allocator.alloc(u8, 17);
+        defer testing.allocator.free(storage);
 
-    {
-        const r = try b.reserve(16);
-        @memcpy(r.data[0..5], "Hello");
-        b.commit(r, 5);
-        try testing.expectEqual(@as(usize, 11), b.totalAvailableForWrite());
-        try testing.expectEqual(@as(usize, 5), b.totalAvailableForRead());
-
-        const peeked = b.peek();
-        try testing.expectEqualStrings("Hello", peeked);
-        b.consume(5);
-    }
-
-    try testing.expectEqual(@as(usize, 16), b.totalAvailableForWrite());
-    try testing.expectEqual(@as(usize, 0), b.totalAvailableForRead());
-
-    {
-        try testing.expectError(error.OutOfMemory, b.reserve(16));
-        const r0 = try b.reserve(11);
-        @memcpy(r0.data[0..8], ", World!");
-        b.commit(r0, 8);
-        try testing.expectEqual(@as(usize, 8), b.totalAvailableForWrite());
-        try testing.expectEqual(@as(usize, 8), b.totalAvailableForRead());
-
-        // This one will leave a mark at 13 because there is only 3 spaces left at the end of the ring.
-        const r1 = try b.reserve(4);
-        @memcpy(r1.data[0..4], "!!!!");
-        b.commit(r1, 4);
-        try testing.expectEqual(@as(usize, 0), b.totalAvailableForWrite());
-        try testing.expectEqual(@as(usize, 12), b.totalAvailableForRead());
-
-        const p1 = b.peek();
-        try testing.expectEqualStrings(", World!", p1);
-        b.consume(2);
-        try testing.expectEqual(@as(usize, 2), b.totalAvailableForWrite());
-        try testing.expectEqual(@as(usize, 10), b.totalAvailableForRead());
-
-        const p2 = b.peek();
-        try testing.expectEqualStrings("World!", p2);
-        // This bumps up tail past the mark and wraps it around, so we recover the full buffer for writes past it.
-        b.consume(p2.len);
-        try testing.expectEqual(@as(usize, 12), b.totalAvailableForWrite());
-        try testing.expectEqual(@as(usize, 4), b.totalAvailableForRead());
-
-        const p3 = b.peek();
-        try testing.expectEqualStrings("!!!!", p3);
-        b.consume(p3.len);
+        var b = B.init(storage);
         try testing.expectEqual(@as(usize, 16), b.totalAvailableForWrite());
         try testing.expectEqual(@as(usize, 0), b.totalAvailableForRead());
 
-        try testing.expectEqual(@as(usize, 13), b.mark);
-        try testing.expectEqual(@as(usize, 4), b.head);
-        try testing.expectEqual(@as(usize, 4), b.tail);
+        {
+            const r = try b.reserve(16);
+            @memcpy(r.data[0..5], "Hello");
+            b.commit(r, 5);
+            try testing.expectEqual(@as(usize, 11), b.totalAvailableForWrite());
+            try testing.expectEqual(@as(usize, 5), b.totalAvailableForRead());
+
+            const peeked = b.peek();
+            try testing.expectEqualStrings("Hello", peeked);
+            b.consume(5);
+        }
+
+        try testing.expectEqual(@as(usize, 16), b.totalAvailableForWrite());
+        try testing.expectEqual(@as(usize, 0), b.totalAvailableForRead());
+
+        {
+            try testing.expectError(error.OutOfMemory, b.reserve(16));
+            const r0 = try b.reserve(11);
+            @memcpy(r0.data[0..8], ", World!");
+            b.commit(r0, 8);
+            try testing.expectEqual(@as(usize, 8), b.totalAvailableForWrite());
+            try testing.expectEqual(@as(usize, 8), b.totalAvailableForRead());
+
+            // This one will leave a mark at 13 because there is only 3 spaces left at the end of the ring.
+            const r1 = try b.reserve(4);
+            @memcpy(r1.data[0..4], "!!!!");
+            b.commit(r1, 4);
+            try testing.expectEqual(@as(usize, 0), b.totalAvailableForWrite());
+            try testing.expectEqual(@as(usize, 12), b.totalAvailableForRead());
+
+            const p1 = b.peek();
+            try testing.expectEqualStrings(", World!", p1);
+            b.consume(2);
+            try testing.expectEqual(@as(usize, 2), b.totalAvailableForWrite());
+            try testing.expectEqual(@as(usize, 10), b.totalAvailableForRead());
+
+            const p2 = b.peek();
+            try testing.expectEqualStrings("World!", p2);
+            // This bumps up tail past the mark and wraps it around, so we recover the full buffer for writes past it.
+            b.consume(p2.len);
+            try testing.expectEqual(@as(usize, 12), b.totalAvailableForWrite());
+            try testing.expectEqual(@as(usize, 4), b.totalAvailableForRead());
+
+            const p3 = b.peek();
+            try testing.expectEqualStrings("!!!!", p3);
+            b.consume(p3.len);
+            try testing.expectEqual(@as(usize, 16), b.totalAvailableForWrite());
+            try testing.expectEqual(@as(usize, 0), b.totalAvailableForRead());
+
+            try testing.expectEqual(@as(usize, 13), b.mark);
+            try testing.expectEqual(@as(usize, 4), b.head);
+            try testing.expectEqual(@as(usize, 4), b.tail);
+        }
     }
 }
