08fb294d81162123933cc644df0bb18062e3cc33
Luis Cáceres <lacc97@protonmail.ch>
2024-03-12 23:12:03 +0000

    Split mark/head/tail logic into separate struct
    This should make it easier to implement more interesting
    variants of the BipBuffer (like a MultiBipBuffer).

--- src/root.zig
+++ src/root.zig
@@ -7,175 +7,190 @@ pub const Options = struct {
     single_threaded: bool = builtin.single_threaded,
 };
 
-pub fn BipBufferUnmanaged(comptime T: type, comptime opts: Options) type {
+fn BipBufferCore(comptime opts: Options) type {
     return struct {
-        const Buffer = @This();
+        const BufferCore = @This();
 
-        data: []T,
-        mark: usize,
-        head: usize,
-        tail: usize,
+        mark: usize = 0,
+        head: usize = 0,
+        tail: usize = 0,
 
-        pub fn init(buf: []T) Buffer {
-            if (!(buf.len > 0)) @panic("empty buffer");
-
-            return .{
-                .data = buf,
-                .mark = 0,
-                .head = 0,
-                .tail = 0,
-            };
-        }
-
-        pub fn reset(b: *Buffer) void {
-            const buf = b.data;
-            b.* = init(buf);
-        }
-
-        pub fn reserveExact(b: *Buffer, count: usize) ?Reserve {
-            const r = b.reserveLargest(count);
-            return if (r.data.len == count) r else null;
-        }
-
-        pub fn reserveLargest(b: *Buffer, count: usize) Reserve {
+        fn reserveLargest(b: *BufferCore, count: usize, buffer_len: usize) Reserve {
             // We are the writing thread so we can load head unordered.
             const head = load(&b.head, .unordered);
-            check(head < b.data.len, "BUG: inconsistent head pointer value");
+            check(head < buffer_len, "BUG: inconsistent head pointer value");
 
             // Load acquire to synchronise with the reading thread.
             const tail = load(&b.tail, .acquire);
-            check(tail < b.data.len, "BUG: inconsistent tail pointer value");
+            check(tail < buffer_len, "BUG: inconsistent tail pointer value");
 
             if (head < tail) {
                 // We are the writing thread so we can load mark unordered.
                 const mark = load(&b.mark, .unordered);
-                check(mark <= b.data.len, "BUG: inconsistent mark pointer value");
+                check(mark <= buffer_len, "BUG: inconsistent mark pointer value");
                 check(head <= mark, "BUG: inconsistent mark and head pointer value");
 
                 const len = @min(count, tail - head - 1);
                 return .{
-                    .data = b.data[head..][0..len],
-                    .__bb = b,
-                    .__head = head,
-                    .__mark = mark,
-                    .__mark_shift = false,
+                    .beg = head,
+                    .len = len,
+                    .core = .{
+                        .bc = b,
+                        .head = head,
+                        .mark = mark,
+                        .mark_shift = false,
+                    },
                 };
             } else {
-                const end = if (tail != 0) (b.data.len) else (b.data.len - 1);
+                const end = if (tail != 0) (buffer_len) else (buffer_len - 1);
                 if ((end - head) >= count) {
                     return .{
-                        .data = b.data[head..][0..count],
-                        .__bb = b,
-                        .__head = head,
-                        .__mark = head,
-                        .__mark_shift = true,
+                        .beg = head,
+                        .len = count,
+                        .core = .{
+                            .bc = b,
+                            .head = head,
+                            .mark = head,
+                            .mark_shift = true,
+                        },
                     };
                 } else {
                     // Wrap around.
                     const len = if (tail > 0) @min(count, tail - 1) else 0;
                     return .{
-                        .data = b.data[0..][0..len],
-                        .__bb = b,
-                        .__head = 0,
-                        .__mark = head,
-                        .__mark_shift = false,
+                        .beg = 0,
+                        .len = len,
+                        .core = .{
+                            .bc = b,
+                            .head = 0,
+                            .mark = head,
+                            .mark_shift = false,
+                        },
                     };
                 }
             }
         }
 
-        pub const Reserve = struct {
-            data: []T,
+        const Reserve = struct {
+            /// Pointer to beginning of reserved buffer
+            beg: usize,
 
-            __bb: *Buffer,
+            /// Length of reserved buffer.
+            len: usize,
+
+            /// Inner (buffer independent) fields of reservation.
+            core: ReserveCore,
+        };
+
+        const ReserveCore = struct {
+            bc: *BufferCore,
 
             /// Copy of the head pointer at the time of reserve.
-            __head: usize,
+            head: usize,
 
             /// The next base value for the mark pointer.
-            __mark: usize,
+            mark: usize,
 
             /// Whether we need to shift the mark pointer at commit.
-            __mark_shift: bool,
+            mark_shift: bool,
 
-            pub fn commit(r: Reserve, count: usize) void {
-                check(count <= r.data.len, "bad commit count");
+            fn commit(r: ReserveCore, count: usize, buffer_len: usize, reserve_len: usize) void {
+                check(count <= reserve_len, "bad commit count");
 
                 if (count == 0) return;
 
-                const next_mark = r.__mark + if (r.__mark_shift) (count) else (0);
+                const next_mark = r.mark + if (r.mark_shift) (count) else (0);
                 const next_head = blk: {
-                    var next = r.__head + count;
-                    if (next == r.__bb.data.len) next = 0;
+                    var next = r.head + count;
+                    if (next == buffer_len) next = 0;
                     break :blk next;
                 };
 
-                check(next_head < r.__bb.data.len, "BUG: inconsistent head pointer value");
-                check(next_mark <= r.__bb.data.len, "BUG: inconsistent mark pointer value");
+                check(next_head < buffer_len, "BUG: inconsistent head pointer value");
+                check(next_mark <= buffer_len, "BUG: inconsistent mark pointer value");
                 check(next_head <= next_mark, "BUG: inconsistent mark and head pointer value");
-                store(&r.__bb.mark, next_mark, .unordered);
-                store(&r.__bb.head, next_head, .release);
+                store(&r.bc.mark, next_mark, .unordered);
+                store(&r.bc.head, next_head, .release);
             }
         };
 
-        pub fn peek(b: *Buffer) Peek {
+        fn peek(b: *BufferCore, buffer_len: usize) Peek {
             // We are the reading thread so we can load tail unordered.
             const tail = load(&b.tail, .unordered);
-            check(tail < b.data.len, "BUG: inconsistent tail pointer value");
+            check(tail < buffer_len, "BUG: inconsistent tail pointer value");
 
             const head = load(&b.head, .acquire);
-            check(head < b.data.len, "BUG: inconsistent head pointer value");
+            check(head < buffer_len, "BUG: inconsistent head pointer value");
 
             if (head >= tail) {
                 return .{
-                    .data = b.data[tail..head],
-                    .__bb = b,
-                    .__tail = tail,
-                    .__wrap = false,
+                    .beg = tail,
+                    .len = head - tail,
+                    .core = .{
+                        .bc = b,
+                        .tail = tail,
+                        .wrap = false,
+                    },
                 };
             } else {
                 // Tail is further than head, which means mark cannot be changed until tail wraps around.
                 // This means loading head acquire (which we already did) is correct.
                 const mark = load(&b.mark, .unordered);
-                check(mark <= b.data.len, "BUG: inconsistent mark pointer value");
+                check(mark <= buffer_len, "BUG: inconsistent mark pointer value");
                 check(head <= mark, "BUG: inconsistent mark and head pointer value");
 
                 return if (tail == mark) .{
-                    .data = b.data[0..head],
-                    .__bb = b,
-                    .__tail = 0,
-                    .__wrap = false,
+                    .beg = 0,
+                    .len = head,
+                    .core = .{
+                        .bc = b,
+                        .tail = 0,
+                        .wrap = false,
+                    },
                 } else .{
-                    .data = b.data[tail..mark],
-                    .__bb = b,
-                    .__tail = tail,
-                    .__wrap = true,
+                    .beg = tail,
+                    .len = mark - tail,
+                    .core = .{
+                        .bc = b,
+                        .tail = tail,
+                        .wrap = true,
+                    },
                 };
             }
         }
-        pub const Peek = struct {
-            data: []T,
 
-            __bb: *Buffer,
+        const Peek = struct {
+            /// Pointer to beginning of peeked buffer.
+            beg: usize,
+
+            /// Length of peeked buffer.
+            len: usize,
+
+            /// Inner (buffer independent) fields of peek.
+            core: PeekCore,
+        };
+
+        const PeekCore = struct {
+            bc: *BufferCore,
 
             // Copy of the tail pointer at the time of peeking.
-            __tail: usize,
+            tail: usize,
 
             // Whether a full consume will trigger wrapping on the tail pointer.
-            __wrap: bool,
+            wrap: bool,
 
-            pub fn consume(p: Peek, count: usize) void {
-                check(count <= p.data.len, "bad consume count");
+            fn consume(p: PeekCore, count: usize, buffer_len: usize, peek_len: usize) void {
+                check(count <= peek_len, "bad consume count");
 
                 if (count == 0) return;
 
                 const next_tail = blk: {
-                    var next = p.__tail + count;
-                    if (p.__wrap and count == p.data.len) next = 0;
+                    var next = p.tail + count;
+                    if (p.wrap and count == peek_len) next = 0;
                     break :blk next;
                 };
-                check(next_tail < p.__bb.data.len, "BUG: inconsistent tail pointer value");
-                store(&p.__bb.tail, next_tail, .release);
+                check(next_tail < buffer_len, "BUG: inconsistent tail pointer value");
+                store(&p.bc.tail, next_tail, .release);
             }
         };
 
@@ -203,12 +218,74 @@ pub fn BipBufferUnmanaged(comptime T: type, comptime opts: Options) type {
     };
 }
 
+pub fn BipBuffer(comptime T: type, comptime opts: Options) type {
+    return struct {
+        const Buffer = @This();
+
+        data: []T,
+        core: Core = .{},
+
+        const Core = BipBufferCore(opts);
+
+        pub fn init(buf: []T) Buffer {
+            if (!(buf.len > 0)) @panic("empty buffer");
+
+            return .{ .data = buf };
+        }
+
+        pub fn reset(b: *Buffer) void {
+            b.core = .{};
+        }
+
+        pub fn reserveExact(b: *Buffer, count: usize) ?Reserve {
+            const r = b.reserveLargest(count);
+            return if (r.data.len == count) r else null;
+        }
+
+        pub fn reserveLargest(b: *Buffer, count: usize) Reserve {
+            const r = b.core.reserveLargest(count, b.data.len);
+            return .{ .data = b.data[r.beg..][0..r.len], .__core = r.core };
+        }
+
+        pub const Reserve = struct {
+            data: []T,
+            __core: Core.ReserveCore,
+
+            pub fn commit(r: Reserve, count: usize) void {
+                r.__core.commit(
+                    count,
+                    @fieldParentPtr(Buffer, "core", r.__core.bc).data.len,
+                    r.data.len,
+                );
+            }
+        };
+
+        pub fn peek(b: *Buffer) Peek {
+            const p = b.core.peek(b.data.len);
+            return .{ .data = b.data[p.beg..][0..p.len], .__core = p.core };
+        }
+
+        pub const Peek = struct {
+            data: []T,
+            __core: Core.PeekCore,
+
+            pub fn consume(p: Peek, count: usize) void {
+                p.__core.consume(
+                    count,
+                    @fieldParentPtr(Buffer, "core", p.__core.bc).data.len,
+                    p.data.len,
+                );
+            }
+        };
+    };
+}
+
 test {
     const testing = std.testing;
 
     const types = .{
-        BipBufferUnmanaged(u8, .{ .single_threaded = false }),
-        BipBufferUnmanaged(u8, .{ .single_threaded = true }),
+        BipBuffer(u8, .{ .single_threaded = false }),
+        BipBuffer(u8, .{ .single_threaded = true }),
     };
 
     inline for (types) |B| {
@@ -251,9 +328,9 @@ test {
             try testing.expectEqualStrings("!!!!", p3.data);
             p3.consume(p3.data.len);
 
-            try testing.expectEqual(@as(usize, 14), b.mark);
-            try testing.expectEqual(@as(usize, 4), b.head);
-            try testing.expectEqual(@as(usize, 4), b.tail);
+            try testing.expectEqual(@as(usize, 14), b.core.mark);
+            try testing.expectEqual(@as(usize, 4), b.core.head);
+            try testing.expectEqual(@as(usize, 4), b.core.tail);
         }
     }
 }
@@ -264,7 +341,7 @@ test "multithreaded" {
     const iteration_count = 100_000_000;
     const bb_size = 600;
 
-    const BB = BipBufferUnmanaged(u8, .{ .single_threaded = false });
+    const BB = BipBuffer(u8, .{ .single_threaded = false });
 
     const producer = struct {
         fn producer(bb: *BB, arg_rng_state: std.Random.DefaultPrng, count: usize) !void {
