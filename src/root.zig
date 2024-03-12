9c9b17dda270f9e4c983d9ee412e6d1f77f57fe4
Luis Cáceres <lacc97@protonmail.ch>
2024-03-12 23:24:23 +0000

    Allow multiple commit/consume
    The Peek and Reserve objects now mutate.

--- src/root.zig
+++ src/root.zig
@@ -94,7 +94,7 @@ fn BipBufferCore(comptime opts: Options) type {
             /// Whether we need to shift the mark pointer at commit.
             mark_shift: bool,
 
-            fn commit(r: ReserveCore, count: usize, buffer_len: usize, reserve_len: usize) void {
+            fn commit(r: *ReserveCore, count: usize, buffer_len: usize, reserve_len: usize) void {
                 check(count <= reserve_len, "bad commit count");
 
                 if (count == 0) return;
@@ -109,6 +109,9 @@ fn BipBufferCore(comptime opts: Options) type {
                 check(next_head < buffer_len, "BUG: inconsistent head pointer value");
                 check(next_mark <= buffer_len, "BUG: inconsistent mark pointer value");
                 check(next_head <= next_mark, "BUG: inconsistent mark and head pointer value");
+
+                r.mark = next_mark;
+                r.head = next_head;
                 store(&r.bc.mark, next_mark, .unordered);
                 store(&r.bc.head, next_head, .release);
             }
@@ -179,7 +182,7 @@ fn BipBufferCore(comptime opts: Options) type {
             // Whether a full consume will trigger wrapping on the tail pointer.
             wrap: bool,
 
-            fn consume(p: PeekCore, count: usize, buffer_len: usize, peek_len: usize) void {
+            fn consume(p: *PeekCore, count: usize, buffer_len: usize, peek_len: usize) void {
                 check(count <= peek_len, "bad consume count");
 
                 if (count == 0) return;
@@ -189,7 +192,10 @@ fn BipBufferCore(comptime opts: Options) type {
                     if (p.wrap and count == peek_len) next = 0;
                     break :blk next;
                 };
+
                 check(next_tail < buffer_len, "BUG: inconsistent tail pointer value");
+
+                p.tail = next_tail;
                 store(&p.bc.tail, next_tail, .release);
             }
         };
@@ -251,12 +257,13 @@ pub fn BipBuffer(comptime T: type, comptime opts: Options) type {
             data: []T,
             __core: Core.ReserveCore,
 
-            pub fn commit(r: Reserve, count: usize) void {
+            pub fn commit(r: *Reserve, count: usize) void {
                 r.__core.commit(
                     count,
                     @fieldParentPtr(Buffer, "core", r.__core.bc).data.len,
                     r.data.len,
                 );
+                r.data = r.data[count..];
             }
         };
 
@@ -269,12 +276,13 @@ pub fn BipBuffer(comptime T: type, comptime opts: Options) type {
             data: []T,
             __core: Core.PeekCore,
 
-            pub fn consume(p: Peek, count: usize) void {
+            pub fn consume(p: *Peek, count: usize) void {
                 p.__core.consume(
                     count,
                     @fieldParentPtr(Buffer, "core", p.__core.bc).data.len,
                     p.data.len,
                 );
+                p.data = p.data[count..];
             }
         };
     };
@@ -295,36 +303,36 @@ test {
         var b = B.init(storage);
 
         {
-            const r = b.reserveExact(16) orelse return error.OutOfSpace;
+            var r = b.reserveExact(16) orelse return error.OutOfSpace;
             @memcpy(r.data[0..5], "Hello");
             r.commit(5);
 
-            const peeked = b.peek();
+            var peeked = b.peek();
             try testing.expectEqualStrings("Hello", peeked.data);
             peeked.consume(5);
         }
 
         {
             try testing.expectEqual(@as(?B.Reserve, null), b.reserveExact(16));
-            const r0 = b.reserveExact(11) orelse return error.OutOfSpace;
+            var r0 = b.reserveExact(11) orelse return error.OutOfSpace;
             @memcpy(r0.data[0..9], ", World!!");
             r0.commit(9);
 
             // This one will leave a mark at 14 because there is only 3 spaces left at the end of the ring.
-            const r1 = b.reserveExact(4) orelse return error.OutOfSpace;
+            var r1 = b.reserveExact(4) orelse return error.OutOfSpace;
             @memcpy(r1.data[0..4], "!!!!");
             r1.commit(4);
 
-            const p1 = b.peek();
+            var p1 = b.peek();
             try testing.expectEqualStrings(", World!!", p1.data);
             p1.consume(2);
 
-            const p2 = b.peek();
+            var p2 = b.peek();
             try testing.expectEqualStrings("World!!", p2.data);
             // This bumps up tail past the mark and wraps it around, so we recover the full buffer for writes past it.
             p2.consume(p2.data.len);
 
-            const p3 = b.peek();
+            var p3 = b.peek();
             try testing.expectEqualStrings("!!!!", p3.data);
             p3.consume(p3.data.len);
 
@@ -352,7 +360,7 @@ test "multithreaded" {
             while (i < count) {
                 const reserve_count = rng.intRangeLessThan(usize, 1, bb_size / 2);
 
-                const reservation = spin: while (true) break :spin bb.reserveExact(reserve_count) orelse continue :spin;
+                var reservation = spin: while (true) break :spin bb.reserveExact(reserve_count) orelse continue :spin;
                 for (reservation.data, 0..) |*out, j| out.* = @truncate(i + j);
 
                 const commit_count = rng.intRangeLessThan(usize, 0, reserve_count);
@@ -370,7 +378,7 @@ test "multithreaded" {
 
             var i: usize = 0;
             while (i < count) {
-                const peeked = spin: while (true) {
+                var peeked = spin: while (true) {
                     const p = bb.peek();
                     if (p.data.len > 0) break :spin p;
                 };
